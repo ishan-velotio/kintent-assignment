d0e8b63d56ed7c69b81fe1faffcecb4305c61f5e
ishan-velotio <ishan.khanduja@velotio.com>
2022-03-13 15:51:25 +0530

    Get & Update endpoints added for Books

--- api/controllers/book.ts
+++ api/controllers/book.ts
@@ -13,13 +13,93 @@ export const create = async (request, response) => {
   logger.debug(`Request body - ${JSON.stringify(requestBody)}`);
 
   try {
-    const author = await bookService.create(requestBody);
+    const book = await bookService.create(requestBody);
     logger.info('Sending response to the client.');
 
-    return response.status(201).json(author);
+    return response.status(201).json(book);
   } catch (error) {
     logger.info('Something went wrong.');
 
+    return response.status(error.statusCode).json(error.statusMessage);
+  }
+};
+
+export const edit = async (request, response) => {
+  logger.info('Received request to update a book.');
+  
+  const bookId: number = request.swagger.params.id.value;
+  const requestBody: IBookRequest =
+    request.swagger.params.body.value;
+  
+  logger.debug(`Request body - ${JSON.stringify(requestBody)}`);
+  
+  try {
+    const book = await bookService.update(bookId, requestBody);
+    logger.info('Sending response to the client.');
+  
+    return response.status(201).json(book);
+  } catch (error) {
+    logger.info('Something went wrong.');
+  
+    return response.status(error.statusCode).json(error.statusMessage);
+  }
+};
+
+export const get = async (request, response) => {
+  logger.info('Received request to get a book.');
+  
+  const bookId: number = request.swagger.params.id.value;
+  
+  try {
+    const book = await bookService.get(bookId);
+    logger.info('Sending response to the client.');
+  
+    return response.status(200).json(book);
+  } catch (error) {
+    logger.info('Something went wrong.');
+  
+    return response.status(error.statusCode).json(error.statusMessage);
+  }
+};
+
+export const getAll = async (request, response) => {
+  logger.info('Received request to get all books.');
+
+  const searchString: string = request.swagger.params.q.value;
+  const limit: number = request.swagger.params.limit.value;
+  const offset: number = request.swagger.params.offset.value;
+
+  const searchParams: IBookSearchParam = {
+    searchString,
+    limit,
+    offset,
+  }
+
+  try {
+    const books = await bookService.getAll(searchParams);
+    logger.info('Sending response to the client.');
+
+    return response.status(200).json(books);
+  } catch (error) {
+    logger.info('Something went wrong.');
+
+    return response.status(error.statusCode).json(error.statusMessage);
+  }
+};
+
+export const deleteBook = async (request, response) => {
+  logger.info('Received request to delete a book.');
+    
+  const bookId: number = request.swagger.params.id.value;
+    
+  try {
+    const res = await bookService.delete(bookId);
+    logger.info('Sending response to the client.');
+    
+    return response.status(200).json(res);
+  } catch (error) {
+    logger.info('Something went wrong.');
+    
     return response.status(error.statusCode).json(error.statusMessage);
   }
 };
--- api/service/book.ts
+++ api/service/book.ts
@@ -2,6 +2,7 @@ import * as logger from 'winston';
 
 import BookRepository from '../dataAccess/book';
 import AuthorRepository from '../dataAccess/author';
+import db from '../models';
 
 export default class BookService {
   private bookRepo: BookRepository;
@@ -11,7 +12,7 @@ export default class BookService {
     this.authorRepo = new AuthorRepository();
   }
 
-  public async create( bookReq: IBookRequest ) {
+  public async create( bookReq: IBookRequest ): Promise<IBook> {
     logger.info('Service called to create the book');
 
     const bookAttrs = this.toAttributeObj(bookReq);
@@ -29,6 +30,73 @@ export default class BookService {
     return this.toResponseObj(book, bookAuthors);
   }
 
+  public async get( bookId: number ): Promise<IBook> {
+    logger.info('Service called to get a book');
+
+    const book = await this.bookRepo.getById(bookId);
+    const bookAuthors = await book.getAuthors();
+
+    return this.toResponseObj(book, bookAuthors);
+  }
+
+  public async getAll(searchParams: IBookSearchParam): Promise<IBook[]> {
+    logger.info('Service called to get all books');
+
+    const {whereClause, limit, offset} = this.getSearchOptions(searchParams);
+
+    const books = await this.bookRepo.getAll(whereClause, limit, offset);
+    const response: IBook[] = [];
+    for (const book of books) {
+      const bookAuthors = await book.getAuthors();
+      response.push(this.toResponseObj(book, bookAuthors));
+    }
+
+    return response;
+  }
+
+  public async update( bookId: number, bookReq: IBookRequest ): Promise<IBook> {
+    logger.info('Service called to update a book');
+
+    const bookAttrs = this.toAttributeObj(bookReq);
+    const bookUpdateObj = await this.bookRepo.update(bookId, bookAttrs);
+    const book = bookUpdateObj[1][0];
+    if (bookReq.authorIds.length) {
+      const authors = await this.authorRepo.getByIds(bookReq.authorIds);
+      await this.bookRepo.updateBookAuthor(book, authors);
+      logger.info('Book Authors Updated');
+    } else {
+      await this.bookRepo.updateBookAuthor(book, []);
+    }
+
+    const bookAuthors = await book.getAuthors();
+
+    return this.toResponseObj(book, bookAuthors);
+  }
+
+  public async delete( bookId: number ) {
+    logger.info('Service called to delete a book');
+
+    await this.bookRepo.delete(bookId);
+    logger.info('Book deleted');
+  }
+
+  private getSearchOptions(searchParams: IBookSearchParam) {
+    let whereClause = {};
+
+    console.log(searchParams);
+
+    if (searchParams.searchString && searchParams.searchString.trim().length) {
+      whereClause = { ...whereClause, name: {[db.Sequelize.Op.iLike]: `%${searchParams.searchString}%`} };
+      console.log('Here')
+    }
+
+    console.log(whereClause);
+
+    const limit: number = searchParams.limit || null;
+    const offset: number = searchParams.offset || null;
+
+    return { whereClause, limit, offset };
+  }
 
   private toAttributeObj(book: IBookRequest): IBookAttributes {
     return {
